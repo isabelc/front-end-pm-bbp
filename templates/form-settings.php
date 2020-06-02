62df2ecf55124d4d7a9040982d8790e23aa8ac93
Isabel <isabel@isabelcastillo.com>
2020-06-01 21:09:44 -0400

    better Settings heading

--- templates/form-settings.php
+++ templates/form-settings.php
@@ -3,6 +3,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 ?>
-<h2><?php esc_html_e( 'Set your preferences below', 'front-end-pm' ); ?></h2>
+<h2>Set your preferences</h2>
 <?php echo fep_info_output(); ?>
 <?php echo Fep_Form::init()->form_field_output( 'settings' ); ?>
