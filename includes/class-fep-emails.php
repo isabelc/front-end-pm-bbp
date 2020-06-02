29b1eb9fdae5f32bf94ea212f416135bc3813926
Isabel <isabel@isabelcastillo.com>
2020-06-02 17:42:22 -0400

    better email message style

--- includes/class-fep-emails.php
+++ includes/class-fep-emails.php
@@ -36,32 +36,32 @@ class Fep_Emails {
 			$subject  = get_bloginfo( 'name' ) . ': ' . __( 'New Message', 'front-end-pm' );
 
 
-			$pstyle = " style='width:500px;max-width:100%;font-family:Arial,\"Helvetica Neue\",Helvetica,sans-serif;font-size:16px;color:#444;line-height:1.6'";
+			$stylefont = "font-family:Arial,\"Helvetica Neue\",Helvetica,sans-serif;";
 
-			$message  = "<div style='background:#f2f6f7;padding:24px;'><div style='width:600px;max-width:90%;font-family:Arial,\"Helvetica Neue\",Helvetica,sans-serif;font-size:16px;color:#444;border:1px solid #ececec;line-height:1.6;background-color: #ffffff;margin-left:auto;margin-right:auto;padding:0 24px;'><div style='text-align:center'>";
+			$stylealign = "width:600px;max-width:90%;margin-left:auto;margin-right:auto;border:1px solid #ececec;";
 
+			$pstyle = " style='width:500px;max-width:100%;" . $stylefont . "font-size:16px;color:#444;line-height:1.6'";
 
-			/****************************************************
-			* @todo must update image url:
-			* make sure it's the right color (lighter?) and minified
-			http://localhost/wp-content/themes/forumpress/img/astrotalk-lighter.svg
-
-			****************************************************/
-
-			// $message .= "<img alt='letterhead' src='http://localhost/wp-content/themes/forumpress/img/astrotalk-lighter.svg' width='160' height='55'>";// @todo do this live!!
-
-
-			$message .= "</div><p $pstyle>You have received a new message at " . get_bloginfo( 'name' ) . '.</p>';
-			$message .= '<p $pstyle>From: <strong>' . fep_user_name( $mgs->mgs_author ) . '</strong><br>';
-			$message .= 'Subject: <strong>' . $mgs->mgs_title . '</strong></p>';
-			$message .= '<br><a style="font-weight:700;border-radius: 4px;color: #fff;background-color: #7F54B3;text-decoration:none;padding: 10px 15px;white-space: nowrap;" href="' . fep_query_url( 'messagebox' ) . '">See Message</a><br><br></div>';
-
-			// @todo live update localhost url to live one: http://localhost/messages/?fepaction=settings
-
-			$message .= "<div style='border-top: 1px solid #f3f3f3; color: #808080;width:600px;max-width:90%;font-family:Arial,\"Helvetica Neue\",Helvetica,sans-serif;font-size: 13px; margin-left:auto;margin-right:auto;padding:14px 24px;'>You're receiving this email because you allow messages at <a href='" .
-				home_url() ."' style='color:#7f54b3;text-decoration: underline;'>" .
-				get_bloginfo( 'name' ) .".org</a>." .
-				"<br>Change your email preferences at <a style='color:#7f54b3;text-decoration:underline;' href='" . fep_query_url( 'settings' ) . "'>Message Settings</a>.</div></div>";
+			$message  = "<div style='background:#f2f6f7;padding:24px;'>" .
+						"<div style='" . $stylealign . $stylefont .
+						"font-size:16px;color:#444;line-height:1.6;background-color: #ffffff;padding:0 24px;'>" .
+						"<div style='text-align:center'>" . get_custom_logo() . "</div>" .
+						"<p $pstyle>You have received a new message at " . get_bloginfo( 'name' ) . '.</p>' .
+						'<p $pstyle>From: <strong>' . fep_user_name( $mgs->mgs_author ) . '</strong><br>' .
+						'Subject: <strong>' . $mgs->mgs_title . '</strong></p>' .
+						'<br>' .
+						'<a style="font-weight:700;border-radius:4px;color:#fff;background-color: #7F54B3;text-decoration:none;padding:10px 15px;white-space:nowrap;" href="' . fep_query_url('messagebox') . '">See Message</a>' .
+						'<br><br>' .
+						'</div>' .
+						"<div style='color: #808080;" . $stylealign . $stylefont .
+						"font-size: 13px;padding:14px 24px;'>" .
+						"You're receiving this email because you allow messages at <a href='" .	home_url() .
+						"' style='color:#7f54b3;text-decoration: underline;'>" .
+						get_bloginfo( 'name' ) .".org</a>." .
+						"<br>" .
+						"Change your email preferences at " .
+						"<a style='color:#7f54b3;text-decoration:underline;' href='" . fep_query_url('settings') . "'>Message Settings</a>." .
+						"</div></div>";
 
 			if ( 'html' == fep_get_option( 'email_content_type', 'plain_text' ) ) {
 				$content_type = 'text/html';
